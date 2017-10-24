4e34b0d490085f221ed9e8fc752d462a8d5d4995
Pablo Sanchez Egido <pablosze@gmail.com>
2017-10-24 18:09:50 +0200

    Created methods for generating XML document and write it to a file and generate JSON string and write it to a file

--- src/rssdom/RSSDom.java
+++ src/rssdom/RSSDom.java
@@ -3,13 +3,22 @@ package rssdom;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This program takes a RSS URL feed as input, processes it by using DOM
@@ -21,7 +30,12 @@ public class RSSDom {
 
     private static final String RSS_URL = "http://www.europapress.es/rss/rss.aspx";
 
-    private RSSDom() {}
+    private String channelTitle;
+    private List<String> newsTitles;
+
+    private RSSDom() {
+        newsTitles = new ArrayList<>();
+    }
 
     public static void main(String[] args) {
         new RSSDom().onInitialize();
@@ -29,9 +43,12 @@ public class RSSDom {
 
     private void onInitialize() {
         try {
-            Document doc = getDocumentFromUrl(RSS_URL);
-            printChannelInfo(doc);
-            printChannelNews(doc);
+            Document docIn = getDocumentFromUrl(RSS_URL);
+            printChannelInfo(docIn);
+            printChannelNews(docIn);
+            Document docOut = generateXMLDoc(getDocumentFromUrl(""));
+            writeXMLToFile(docOut);
+            writeJSONToFile(generateJSON());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -41,23 +58,23 @@ public class RSSDom {
      * This method takes an URL and returns a {@link Document} ready to be processed
      * @param url The RSS URL
      * @return The document loaded as a DOM
-     * @throws ParserConfigurationException
-     * @throws IOException
-     * @throws SAXException
+     * @throws ParserConfigurationException ParserConfigurationException
+     * @throws IOException IOException
+     * @throws SAXException SAXException
      */
-    public Document getDocumentFromUrl(String url) throws ParserConfigurationException, IOException, SAXException {
-        if (url.isEmpty()) {
-            throw new IllegalArgumentException("URL must not be empty");
-        }
-
-        if (!url.startsWith("http")) {
+    private Document getDocumentFromUrl(String url) throws ParserConfigurationException, IOException, SAXException {
+        if (!url.isEmpty() && !url.startsWith("http")) {
             throw new IllegalArgumentException("URL must start by \"http\"");
         }
 
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         dbf.setIgnoringElementContentWhitespace(true);
         DocumentBuilder db = dbf.newDocumentBuilder();
-        return db.parse(new URL(url).openStream());
+        if (url.isEmpty()) {
+            return db.newDocument();
+        } else {
+            return db.parse(new URL(url).openStream());
+        }
     }
 
     /**
@@ -73,7 +90,8 @@ public class RSSDom {
         Element channel = (Element) doc.getElementsByTagName("channel").item(0);
         if (channel != null) {
             Element title = (Element) channel.getElementsByTagName("title").item(0);
-            print("\tTitulo: " + getTextFromNode(title));
+            channelTitle = getTextFromNode(title);
+            print("\tTitulo: " + channelTitle);
             Element url = (Element) channel.getElementsByTagName("link").item(0);
             print("\tUrl: " + getTextFromNode(url));
             Element description = (Element) channel.getElementsByTagName("description").item(0);
@@ -96,7 +114,9 @@ public class RSSDom {
             print("\tNoticia " + (i+1));
             Element currentNew = (Element) news.item(i);
             Element title = (Element) currentNew.getElementsByTagName("title").item(0);
-            print("\t\tTitulo: " + getTextFromNode(title));
+            String titleText = getTextFromNode(title);
+            newsTitles.add(titleText);
+            print("\t\tTitulo: " + titleText);
             Element url = (Element) currentNew.getElementsByTagName("link").item(0);
             print("\t\tUrl: " + getTextFromNode(url));
             Element description = (Element) currentNew.getElementsByTagName("description").item(0);
@@ -105,6 +125,100 @@ public class RSSDom {
             print("\t\tFecha de publicacion: " + getTextFromNode(pubDate));
             Element category = (Element) currentNew.getElementsByTagName("category").item(0);
             print("\t\tCategoria: " + getTextFromNode(category));
+        }
+    }
+
+    /**
+     * This method receives an empty document and generates another document
+     * with the following format:
+     * <noticias canal="titulo canal">
+     *     <noticia>titulo noticia</noticia>
+     * </noticias>
+     * @param doc The document to process
+     * @return The XML document ready to be written on a file
+     */
+    private Document generateXMLDoc(Document doc) {
+        if (doc == null) {
+            throw new IllegalArgumentException("Document must not be null");
+        }
+
+        Element noticias = doc.createElement("noticias");
+        noticias.setAttribute("canal", channelTitle);
+        doc.appendChild(noticias);
+
+        for (String title : newsTitles) {
+            Element noticia = doc.createElement("noticia");
+            Text newTitle = doc.createTextNode(title);
+            noticia.appendChild(newTitle);
+            noticias.appendChild(noticia);
+        }
+
+        return doc;
+    }
+
+    /**
+     * This method receives an XML document and writes it to a file
+     * @param doc The document to process
+     * @throws TransformerException TransformerException
+     */
+    private void writeXMLToFile(Document doc) throws TransformerException {
+        if (doc == null) {
+            throw new IllegalArgumentException("Document must not be null");
+        }
+
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+
+        transformer.setOutputProperty(OutputKeys.VERSION, "1.0");
+        transformer.setOutputProperty(OutputKeys.ENCODING, "utf-8");
+        transformer.setOutputProperty(OutputKeys.STANDALONE, "yes");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+        transformer.transform(new DOMSource(doc),
+                new StreamResult(new File("noticias_" + channelTitle + ".xml")));
+    }
+
+    /**
+     * This methods generates a JSON string with the following format:
+     * {"noticias":
+     *   [{"noticia": "titulo noticia"},
+     *    {"noticia": "titulo noticia"}
+     *    ...
+     *   ]
+     * }
+     * @return The JSON
+     */
+    private String generateJSON() {
+        StringBuilder json = new StringBuilder("{\"noticias\":");
+
+        if (newsTitles.size() > 0) {
+            json.append("[");
+            for (int i = 0; i < newsTitles.size() - 1; i++) {
+                String title = newsTitles.get(i);
+                if (title.contains("\"")) {
+                    title = title.replace("\"", "\\\"");
+                }
+                json.append("{\"noticia\": \"").append(title).append("\"},");
+            }
+            json.append("{\"noticia\": \"").append(newsTitles.get(newsTitles.size() - 1)).append("\"}]");
+        } else {
+            json.append("[]");
+        }
+
+        return json.append("}").toString();
+    }
+
+    /**
+     * This method takes a JSON string and writes it to a file
+     * @param json The JSON string to write on a file
+     */
+    private void writeJSONToFile(String json) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("noticias_" + channelTitle + ".json"));
+            bw.write(json);
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
