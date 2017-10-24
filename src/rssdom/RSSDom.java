c2004d67c20189b18482b349fbb2ea9f564115ba
Pablo Sanchez Egido <pablosze@gmail.com>
2017-10-24 13:23:40 +0200

    Created a method that receives an URL and returns a Document object

--- src/rssdom/RSSDom.java
+++ src/rssdom/RSSDom.java
@@ -1,8 +1,45 @@
 package rssdom;
 
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.net.URL;
+
 public class RSSDom {
 
+    private static final String RSS_URL = "http://www.europapress.es/rss/rss.aspx";
+
+    private RSSDom() {}
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new RSSDom().onInitialize();
+    }
+
+    private void onInitialize() {
+        try {
+            Document doc = getDocumentFromUrl(RSS_URL);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Document getDocumentFromUrl(String url) throws ParserConfigurationException, IOException, SAXException {
+        if (url.isEmpty()) {
+            throw new IllegalArgumentException("URL must not be empty");
+        }
+
+        if (!url.startsWith("http")) {
+            throw new IllegalArgumentException("URL must start by 'http'");
+        }
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setIgnoringElementContentWhitespace(true);
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        return db.parse(new URL(url).openStream());
     }
 }
